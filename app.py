ca9aa578331d1bcd40b1f95ef050a8ad3538db53
Marco Zausch <jamaza@posteo.de>
2022-06-29 23:18:36 +0200

    second trial

--- app.py
+++ app.py
@@ -17,100 +17,90 @@ external_stylesheets = [
     },
 ]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 app.title = "Avocado Analytics: Understand Your Avocados!"
 
-app.layout = html.Div(
-    children=[
-        html.Div(
-            children=[
-                html.P(children="🥑", className="header-emoji"),
-                html.H1(
-                    children="Avocado Analytics", className="header-title"
+app.layout = html.Div(children=[
+    html.Div(
+        children=[
+            html.P(children="🥑", className="header-emoji"),
+            html.H1(children="Avocado Analytics", className="header-title"),
+            html.P(
+                children="Analyze the behavior of avocado prices"
+                " and the number of avocados sold in the US"
+                " between 2015 and 2018",
+                className="header-description",
+            ),
+        ],
+        className="header",
+    ),
+    html.Div(
+        children=[
+            html.Div(children=[
+                html.Div(children="Region", className="menu-title"),
+                dcc.Dropdown(
+                    id="region-filter",
+                    options=[{
+                        "label": region,
+                        "value": region
+                    } for region in np.sort(data.region.unique())],
+                    value="Albany",
+                    clearable=False,
+                    className="dropdown",
                 ),
-                html.P(
-                    children="Analyze the behavior of avocado prices"
-                    " and the number of avocados sold in the US"
-                    " between 2015 and 2018",
-                    className="header-description",
+            ]),
+            html.Div(children=[
+                html.Div(children="Type", className="menu-title"),
+                dcc.Dropdown(
+                    id="type-filter",
+                    options=[{
+                        "label": avocado_type,
+                        "value": avocado_type
+                    } for avocado_type in data.type.unique()],
+                    value="organic",
+                    clearable=False,
+                    searchable=False,
+                    className="dropdown",
                 ),
-            ],
-            className="header",
-        ),
-        html.Div(
-            children=[
-                html.Div(
-                    children=[
-                        html.Div(children="Region", className="menu-title"),
-                        dcc.Dropdown(
-                            id="region-filter",
-                            options=[
-                                {"label": region, "value": region}
-                                for region in np.sort(data.region.unique())
-                            ],
-                            value="Albany",
-                            clearable=False,
-                            className="dropdown",
-                        ),
-                    ]
+            ], ),
+            html.Div(children=[
+                html.Div(children="Date Range", className="menu-title"),
+                dcc.DatePickerRange(
+                    id="date-range",
+                    min_date_allowed=data.Date.min().date(),
+                    max_date_allowed=data.Date.max().date(),
+                    start_date=data.Date.min().date(),
+                    end_date=data.Date.max().date(),
                 ),
-                html.Div(
-                    children=[
-                        html.Div(children="Type", className="menu-title"),
-                        dcc.Dropdown(
-                            id="type-filter",
-                            options=[
-                                {"label": avocado_type, "value": avocado_type}
-                                for avocado_type in data.type.unique()
-                            ],
-                            value="organic",
-                            clearable=False,
-                            searchable=False,
-                            className="dropdown",
-                        ),
-                    ],
+            ]),
+        ],
+        className="menu",
+    ),
+    html.Div(
+        children=[
+            html.Div(
+                children=dcc.Graph(
+                    id="price-chart",
+                    config={"displayModeBar": False},
                 ),
-                html.Div(
-                    children=[
-                        html.Div(
-                            children="Date Range", className="menu-title"
-                        ),
-                        dcc.DatePickerRange(
-                            id="date-range",
-                            min_date_allowed=data.Date.min().date(),
-                            max_date_allowed=data.Date.max().date(),
-                            start_date=data.Date.min().date(),
-                            end_date=data.Date.max().date(),
-                        ),
-                    ]
+                className="card",
+            ),
+            html.Div(
+                children=dcc.Graph(
+                    id="volume-chart",
+                    config={"displayModeBar": False},
                 ),
-            ],
-            className="menu",
-        ),
-        html.Div(
-            children=[
-                html.Div(
-                    children=dcc.Graph(
-                        id="price-chart",
-                        config={"displayModeBar": False},
-                    ),
-                    className="card",
-                ),
-                html.Div(
-                    children=dcc.Graph(
-                        id="volume-chart",
-                        config={"displayModeBar": False},
-                    ),
-                    className="card",
-                ),
-            ],
-            className="wrapper",
-        ),
-    ]
-)
+                className="card",
+            ),
+        ],
+        className="wrapper",
+    ),
+])
 
 
 @app.callback(
-    [Output("price-chart", "figure"), Output("volume-chart", "figure")],
+    [Output("price-chart", "figure"),
+     Output("volume-chart", "figure")],
     [
         Input("region-filter", "value"),
         Input("type-filter", "value"),
@@ -119,12 +109,10 @@ app.layout = html.Div(
     ],
 )
 def update_charts(region, avocado_type, start_date, end_date):
-    mask = (
-        (data.region == region)
-        & (data.type == avocado_type)
-        & (data.Date >= start_date)
-        & (data.Date <= end_date)
-    )
+    mask = ((data.region == region)
+            & (data.type == avocado_type)
+            & (data.Date >= start_date)
+            & (data.Date <= end_date))
     filtered_data = data.loc[mask, :]
     price_chart_figure = {
         "data": [
@@ -141,8 +129,13 @@ def update_charts(region, avocado_type, start_date, end_date):
                 "x": 0.05,
                 "xanchor": "left",
             },
-            "xaxis": {"fixedrange": True},
-            "yaxis": {"tickprefix": "$", "fixedrange": True},
+            "xaxis": {
+                "fixedrange": True
+            },
+            "yaxis": {
+                "tickprefix": "$",
+                "fixedrange": True
+            },
             "colorway": ["#17B897"],
         },
     }
@@ -156,9 +149,17 @@ def update_charts(region, avocado_type, start_date, end_date):
             },
         ],
         "layout": {
-            "title": {"text": "Avocados Sold", "x": 0.05, "xanchor": "left"},
-            "xaxis": {"fixedrange": True},
-            "yaxis": {"fixedrange": True},
+            "title": {
+                "text": "Avocados Sold",
+                "x": 0.05,
+                "xanchor": "left"
+            },
+            "xaxis": {
+                "fixedrange": True
+            },
+            "yaxis": {
+                "fixedrange": True
+            },
             "colorway": ["#E12D39"],
         },
     }
